d7816b2a44893a5ac2d552786551c6d9a991bbf3
saksham19rawat <sakshamrawatb26@gmail.com>
2025-07-16 17:01:08 +0530

    Update Control_Unit_Top.v

--- Codes/RISCVSCC/Control_Unit_Top.v
+++ Codes/RISCVSCC/Control_Unit_Top.v
@@ -1,6 +1,14 @@
+/*control unit has 2 part named alu and main decoder
+we can directly use both of them for our single cycle top but to reduce complexity run time and easier access we create a
+different top for control unit
+the code for the decoder is not ass difficult but we need to check small errors while 
+declaring the ports as verilog is case sensitive and any errorr in either of decoder or the top accumulates in the final results*/
+
+// calling the 2 modules / integrating
 `include "alu_decoder.v"
 `include "main_decoder.v"
 
+// ports for the control unit
 module Control_Unit_Top(Op,RegWrite,ImmSrc,ALUSrc,MemWrite,ResultSrc,Branch,funct3,funct7,ALUControl);
 
 input [6:0]Op,funct7;
@@ -11,6 +19,7 @@ output [2:0]ALUControl;
 
 wire [1:0]ALUOp;
 
+// calling the modules with their respective ports
 main_decoder main_decoder(
                 .Op(Op),
                 .RegWrite(RegWrite),
